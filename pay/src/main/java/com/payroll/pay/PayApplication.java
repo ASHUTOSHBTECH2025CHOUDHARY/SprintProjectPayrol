0f60b44596222ab8bb3d725f329f7eda990476bd
ashutosh <ashutosh.choudhary_cs21@gla.ac.in>
2025-03-09 15:18:12 +0530

    Setting Enviornmental Variable

--- pay/src/main/java/com/payroll/pay/PayApplication.java
+++ pay/src/main/java/com/payroll/pay/PayApplication.java
@@ -1,13 +1,18 @@
 package com.payroll.pay;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
+@Slf4j
 @SpringBootApplication
 public class PayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PayApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(PayApplication.class, args);
+		Environment env = context.getEnvironment();
+		log.info("Active Profile: {}", env.getActiveProfiles().length > 0 ? env.getActiveProfiles()[0] : "default");
 	}
-
 }
